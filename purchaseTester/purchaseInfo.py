0929a92cdc635f5a901a38685f8e69f4c977d657
Hugo Hu <69009840+Hugoyhu@users.noreply.github.com>
2021-09-30 21:29:26 -0400

    Create purchaseInfo.py

---
+++ purchaseTester/purchaseInfo.py
@@ -0,0 +1,27 @@
+senderAddress = {
+    "name": "Hugo Hu",
+    "street1": "Corgi St",
+    "street2": "APT Welsh",
+    "city": "New Corgi",
+    "state": "New Dog",
+    "zip": "CORGI",
+    "country": "US",
+}
+
+recipientAddress = {
+    "name": "Hack Club",
+    "street1": "15 Falls Road",
+    "city": "New York",
+    "state": "NY",
+    "zip": "05482",
+    "country": "US",
+}
+
+parcelInfo = {
+    "length": "6",
+    "width": "4",
+    "height": "2",
+    "distance_unit": "in",
+    "weight": "15",
+    "mass_unit": "oz",
+}
