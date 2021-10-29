007d48fc6eb34d9a25d98560330aac76fbc38642
Hugo Hu <69009840+Hugoyhu@users.noreply.github.com>
2021-10-28 21:25:02 -0400

    Update supabaseAddress.py

--- v0/supabaseAddress.py
+++ v0/supabaseAddress.py
@@ -4,58 +4,95 @@ import supabase_py as sb
 #import supabase_client
 import datetime
 
-url = os.environ.get("SUPABASE_URL")
-key = os.environ.get("SUPABASE_KEY")
-supabase = sb.create_client(url, key)
-
-
-def address_uid(uid):
-    data = supabase.table('addresses').select(
-        'inserted_at, name, uid, addr1, addr2,city, state, zip, country'
+class Address:
+  def __init__(self):
+    url = os.environ.get("SUPABASE_URL")
+    key = os.environ.get("SUPABASE_KEY")
+    self.supabase = sb.create_client(url, key)
+    self.addresses = self.supabase.table('addresses').select(
+    'inserted_at, name, uid, addr1, addr2,city, state, zip, country'
     ).execute()
-    #error, results = (
-    #supabase.table("addresses")
-    #.select("name, uid, addr1, addr2, city, state, zip, countr")
-    #.query()
-    #)
-    #print(results)
 
-    # 2021-10-14T15:44:51.684Z
+  def nodemasters(self, uid):
+    nodemasters = self.supabase.table('nodemasters').select('uid').execute()
+    for nuid in range(len(nodemasters['data'])):
+      if nodemasters['data'][nuid]['uid'] == uid:
+        return True
+    return False
+
+  def admins(self, uid):
+    admins = self.supabase.table('admin').select('uid').execute()
+    for nuid in range(len(admins['data'])):
+      if admins['data'][nuid]['uid'] == uid:
+        return True
+    return False
+
+  def leaders(self, uid):
+    leaders = self.supabase.table('leaders').select('uid').execute()
+    for nuid in range(len(leaders['data'])):
+      if leaders['data'][nuid]['uid'] == uid:
+        return True
+    return False
+
+  def nodemaster_address(self, uid):
+    nodemasters = self.supabase.table('nodemasters').select('name, uid, addr1, addr2, city, state, zip, country').execute()
+    for nuid in range(len(nodemasters['data'])):
+      if nodemasters['data'][nuid]['uid'] == uid:
+        return nodemasters['data'][nuid]
+    return
+
+  def address_uid(self, uid="", ts=""):
     epoch = lambda e: datetime.datetime.fromisoformat(e).timestamp() * 1000
-    if data == None:
-        return
-    recordList = data['data']
+
+    ts_result = ""
+    if ts != "":
+      orders = self.supabase.table('orders').select('shipto_uid, message_id').execute()
+      for record in range(len(orders['data'])):
+        if orders['data'][record]['message_id'] == ts:
+          ts_result = orders['data'][record]['shipto_uid']
+    
+    recordList = self.addresses['data']
+
     returnList = []
-    largerRecord = None
-    print(data)
-    for record in range(len(recordList)):
-        if recordList[record]['uid'] == uid:
-            returnList.append(recordList[record])
-
+    for i in range(len(recordList)):
+      print(recordList[i]['uid'])
+      if recordList[i]['uid'] == uid:
+        print("finally")
+        returnList.append(recordList[i])
+      
     print(returnList)
-    largerRecord = returnList[0]
+    returnListCounter = 0
     for i in range(len(returnList)):
-        epoch_return_list = epoch(returnList[i]["inserted_at"][0:19])
-        epoch_larger_record = epoch(largerRecord["inserted_at"][0:19])
-        if epoch_return_list > epoch_larger_record:
-            largerRecord = returnList[i]
-        i += 1
+      if returnList[i]['name'] != "":
+        break
+      else:
+        returnListCounter+=1
+        if returnListCounter == len(returnList):
+          return {"name": "", "uid": uid, "addr1": "", "addr2": "", "city": "", "state": "", "zip": "", "country": ""}
 
-    return largerRecord
+    if len(returnList) == 0:
+      return
+      
+    largerRecord = None
 
+    for record in range(len(recordList)):
+      if ts_result != "":
+        uid = ts_result
+      if recordList[record]['uid'] == uid:
+        returnList.append(recordList[record])
+    
+      largerRecord = returnList[0]
+      for i in range(len(returnList)):
+          epoch_return_list = epoch(returnList[i]["inserted_at"][0:19])
+          epoch_larger_record = epoch(largerRecord["inserted_at"][0:19])
+          if epoch_return_list > epoch_larger_record:
+              largerRecord = returnList[i]
+          i += 1
 
-def removeDuplicates(uid):
-    record = address_uid(uid)
+      return largerRecord
 
-    #check if record present
-    if record == None:
-        return
-
-    record["uid"] = None
-
-
-def insertAddress(name, uid, addr1, addr2, city, state, zip, country):
-    data = supabase.table("addresses").insert({
+  def insertAddress(self, name, uid, addr1, addr2, city, state, zip, country):
+    data = self.supabase.table("addresses").insert({
         'name': name,
         'uid': uid,
         'addr1': addr1,
@@ -69,9 +106,8 @@ def insertAddress(name, uid, addr1, addr2, city, state, zip, country):
 
     return
 
-
-def addOrder(request, shipto, order, id):
-    data = supabase.table("orders").insert({
+  def addOrder(self, request, shipto, order, id):
+    data = self.supabase.table("orders").insert({
         'request_uid': request,
         'shipto_uid': shipto,
         'order_type': order,
@@ -81,3 +117,28 @@ def addOrder(request, shipto, order, id):
 
     return
 
+  def addLead(self, uid):
+    data = self.supabase.table("leaders").insert({
+        'uid': uid
+    }).execute()
+    assert len(data.get("data", [])) > 0
+
+    return  
+
+  def getOrders(self, uid):
+    data = self.supabase.table("orders").select("shipto_uid, message_id").execute()
+    if len(data['data']) > 3:
+      dataList = data['data'][len(data['data'])-3:len(data['data'])]
+    else:
+      dataList = data['data']
+    
+    return dataList
+
+  def getOrderTo(self, ts):
+    data = self.supabase.table("orders").select("shipto_uid, message_id").execute()
+    for order in range(len(data['data'])):
+      record = data['data'][order]
+      if record["message_id"] == ts:
+        return record['shipto_uid']
+    
+    return
